5b065631f53aa6f76f213e9ef40d3dabf0dd1d8f
appius1 <149104309+appius1@users.noreply.github.com>
2025-04-19 09:07:44 +0545

    Add inventory_management/backend/src/routes/product.routes.js 1745032966265

--- inventory_management/backend/src/routes/product.routes.js
+++ inventory_management/backend/src/routes/product.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct, scanProduct } = require('../controllers/product.controller');
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct, scanProduct, uploadProductImage } = require('../controllers/product.controller');
 const { protect } = require('../middleware/auth.middleware');
+const upload = require('../middleware/localUpload.middleware');
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ router.get('/:id', protect, getProductById);
 router.put('/:id', protect, updateProduct);
 router.delete('/:id', protect, deleteProduct);
 router.post('/scan', protect, scanProduct);
+
+// Upload product image
+router.post('/:id/upload-image', protect, upload.single('image'), uploadProductImage);
 
 module.exports = router;
