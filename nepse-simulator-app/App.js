08e1ddd20528386cd3a4783dc4a08baf21149445
appius1 <149104309+appius1@users.noreply.github.com>
2025-05-03 16:25:24 +0545

    Add nepse-simulator-app/App.js 1746268825170

---
+++ nepse-simulator-app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import MainNavigator from './src/navigation/MainNavigator';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
